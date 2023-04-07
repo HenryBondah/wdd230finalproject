289e85db238a16f5e9aed5a3d7a141dfe74b8127
HenryBondah <bondahdean@gmail.com>
2023-04-07 05:51:16 -0600

    new

--- freshpage/fresh.js
+++ freshpage/fresh.js
@@ -1,41 +1,79 @@
 
-       // Load the fruits data from the JSON file
-       const fruitsUrl = "fruits.json";
-       fetch(fruitsUrl)
-         .then(response => response.json())
-         .then(data => {
-           // Populate the select inputs with the fruits
-           const selectInputs = document.querySelectorAll("select");
-           selectInputs.forEach(selectInput => {
-             const option = document.createElement("option");
-             option.value = "";
-             option.text = "Select a fruit";
-             selectInput.appendChild(option);
+//        // Load the fruits data from the JSON file
+//        const fruitsUrl = "fruits.json";
+//        fetch(fruitsUrl)
+//          .then(response => response.json())
+//          .then(data => {
+//            // Populate the select inputs with the fruits
+//            const selectInputs = document.querySelectorAll("select");
+//            selectInputs.forEach(selectInput => {
+//              const option = document.createElement("option");
+//              option.value = "";
+//              option.text = "Select a fruit";
+//              selectInput.appendChild(option);
              
-             data.forEach(fruit => {
-               const option = document.createElement("option");
-               option.value = fruit.name;
-               option.text = fruit.name;
-               selectInput.appendChild(option);
-             });
-           });
-         });
+//              data.forEach(fruit => {
+//                const option = document.createElement("option");
+//                option.value = fruit.name;
+//                option.text = fruit.name;
+//                selectInput.appendChild(option);
+//              });
+//            });
+//          });
 
 
 
-let urlParam = new URL(window.location);
-let params1  = urlParam.searchParams;
+// let urlParam = new URL(window.location);
+// let params1  = urlParam.searchParams;
+
+// // Remove this when you are done inspecting parameters in the console
+// for (const p of params1) {
+//     console.log(p);
+// }
+
+// document.querySelector('#Name').textContent = params1.get("name");
+// document.querySelector('#Email').textContent = params1.get("email");
+// document.querySelector('#Phone').textContent = params1.get("phone");
+// document.querySelector('#Fruit1').textContent = params1.get("fruit1");
+// document.querySelector('#Fruit2').textContent = params1.get("fruit2");
+// document.querySelector('#Fruit3').textContent = params1.get("fruit3");
+// document.querySelector('#Message').textContent = params1.get("message")
+
+// Load the fruits data from the JSON file
+const fruitsUrl = "fruits.json";
+fetch(fruitsUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Populate the select inputs with the fruits
+    const selectInputs = document.querySelectorAll("select");
+    selectInputs.forEach(selectInput => {
+      const option = document.createElement("option");
+      option.value = "";
+      option.text = "Select a fruit";
+      selectInput.appendChild(option);
+
+      data.forEach(fruit => {
+        const option = document.createElement("option");
+        option.value = fruit.name;
+        option.text = fruit.name;
+        selectInput.appendChild(option);
+      });
+    });
+  });
+const urlParams = new URLSearchParams(window.location.search);
+const urlParam = new URL(window.location);
+const urlparams = urlParam.searchParams;
 
 // Remove this when you are done inspecting parameters in the console
-for (const p of params1) {
-    console.log(p);
+for (const p of urlparams) {
+  console.log(p);
 }
 
-document.querySelector('#Name').textContent = params1.get("name");
-document.querySelector('#Email').textContent = params1.get("email");
-document.querySelector('#Phone').textContent = params1.get("phone");
-document.querySelector('#Fruit1').textContent = params1.get("fruit1");
-document.querySelector('#Fruit2').textContent = params1.get("fruit2");
-document.querySelector('#Fruit3').textContent = params1.get("fruit3");
-document.querySelector('#Message').textContent = params1.get("message")
+document.querySelector('#name').textContent = urlparams.get("name");
+document.querySelector('#email').textContent = urlparams.get("email");
+document.querySelector('#phone').textContent = urlparams.get("phone");
+document.querySelector('#fruit1').textContent = urlparams.get("fruit1");
+document.querySelector('#fruit2').textContent = urlparams.get("fruit2");
+document.querySelector('#fruit3').textContent = urlparams.get("fruit3");
+document.querySelector('#message').textContent = urlparams.get("message");
 
--- scripts/orderconfirmation.js
+++ scripts/orderconfirmation.js
@@ -1,13 +1,24 @@
 
 let url = new URL(window.location);
-let params  = url.searchParams;
+let params = url.searchParams;
 
-// Remove this when you are done inspecting parameters in the console
 for (const p of params) {
-    console.log(p);
+  console.log(p);
 }
 
-document.querySelector('#Fruit1').textContent = params.get("name");
-document.querySelector('#Fruit2').textContent = params.get("food");
-document.querySelector('#Fruit3').textContent = params.get("gender")
-document.querySelector('#message').textContent = params.get("gender")
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const data = JSON.parse(xhr.responseText);
+    document.querySelector('#name').value = params.get('name');
+    document.querySelector('#email').value = params.get('email');
+    document.querySelector('#phone').value = params.get('phone');
+    document.querySelector('#fruit1').value = data.fruit1;
+    document.querySelector('#fruit2').value = data.fruit2;
+    document.querySelector('#fruit3').value = data.fruit3;
+    document.querySelector('#message').value = data.message;
+  }
+};
+xhr.open('GET', '../freshpage/fruits.json');
+xhr.send();
+
